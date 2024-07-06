a51a169c230eda1cc2113d4307a3aacf45fc92de
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-07-06 17:40:10 +0530

    solved nav issue

--- src/_root/components/Hero.jsx
+++ src/_root/components/Hero.jsx
@@ -5,9 +5,9 @@ import { UserButton } from "@clerk/clerk-react";
 const Hero = () => {
   return (
     <>
-      <nav className="fixed z-10 flex items-center justify-between w-full py-6 border-b-2 lg:h-24 top-bar px-11 border-slate_gray bg-near_black max-md:px-6">
+      <nav className="fixed z-10 flex items-center justify-between w-full py-6 border-b-2 lg:h-24 top-bar px-11 border-slate_gray bg-near_black max-md:px-6 max-md:h-[80px]">
         <a href="https://swift-summarizer.vercel.app/" className="flex flex-row items-start justify-between gap-4">
-          <img src={logo} alt="swift_summarizer_logo" className="h-10 max-w-10" />
+          <img src={logo} alt="swift_summarizer_logo" className="w-10 max-md:w-8" />
           <h4 className="text-[27px] font-goia_display_regular text-pastel_green max-md:hidden">Swift Summarizer</h4>
         </a>
         <div className="flex items-center justify-center gap-5">
@@ -19,7 +19,7 @@ const Hero = () => {
           <UserButton 
             appearance={{
               elements: {
-                avatarBox: "h-[38px] w-[38px]"
+                avatarBox: "h-[38px] w-[38px] max-md:w-8 max-md:h-8"
               }
             }}
           />
