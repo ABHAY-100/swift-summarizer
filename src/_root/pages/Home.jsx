de9292db67202e08e64238bf2ddd06682116f1b5
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-07-11 18:35:14 +0530

    Edit home page
    Added a scale element

--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -20,16 +20,16 @@ const Home = () => {
             </div>
         </nav>
 
-        <div className="inline-flex flex-col items-center justify-center max-w-full gap-8">
+        <div className="inline-flex flex-col items-center justify-center max-w-full gap-8 max-[480px]:scale-95">
             <div className="w-full h-fit flex-col justify-start items-center gap-[22px] flex">
                 <h1 className="text-center text-like-yellow font-normal font-goia-medium leading-[86.40px] max-md:leading-[58px] max-md:text-[54px] md:text-[87px] max-sm:px-5">Rapid web <br/>content digests</h1>
                 <h3 className="max-w-[550px] opacity-80 text-center text-white md:text-[22px] font-normal font-goia leading-[30px] max-md:text-[20px] max-md:leading-[28px] mx-4">Swift Summarizer transforms lengthy webpages into concise overviews. Paste URLs, get key points instantly. Streamline online research.</h3>
             </div>
-            <div className="justify-center items-center gap-2.5 inline-flex font-goia-medium text-xl font-normal max-sm:flex-col w-full max-sm:px-5 max-sm:max-w-[330px]">
-                <button onClick={SummarizeNowButton} className="px-[25px] h-[52px] bg-like-yellow rounded-xl justify-center items-center gap-2.5 flex text-near-black max-sm:w-full">
+            <div className="justify-center items-center gap-2.5 inline-flex font-goia-medium text-xl font-normal max-sm:flex-col w-full max-sm:px-5 max-sm:max-w-[320px]">
+                <button onClick={SummarizeNowButton} className="px-[25px] h-[51px] bg-like-yellow rounded-xl justify-center items-center gap-2.5 flex text-near-black max-sm:w-full">
                     Summarize Now
                 </button>
-                <a href="https://github.com/ABHAY-100/swift-summarizer" target="_blank" className="px-[25px] h-[52px] rounded-xl border-2 border-shadow-gray-dark justify-center items-center gap-2.5 flex max-sm:w-full text-white hover:bg-subtle-shadow-gray ease-in-out duration-100">
+                <a href="https://github.com/ABHAY-100/swift-summarizer" target="_blank" className="px-[25px] h-[51px] rounded-xl border-2 border-shadow-gray-dark justify-center items-center gap-2.5 flex max-sm:w-full text-white hover:bg-subtle-shadow-gray ease-in-out duration-100">
                     <span className="opacity-80">Explore Code</span>
                 </a>
             </div>
